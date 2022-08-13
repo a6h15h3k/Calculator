c41c9ac241f3ce04a99883b0ab80c1c5f383bb6a
a6hi <saraffabhishek@outlook.com>
2022-08-13 01:03:54 -0400

    Added Flags and Logic for when User can press Numbers, Operators and Decimal

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 let upperDisplay = document.querySelector("#display-top");
 let lowerDisplay = document.querySelector("#display-bottom");
 let buttons = Array.from(document.querySelectorAll(".button"));
-let result;
+let result = "", newNum, numPressed = false, canPressDecimal = false, canPressOperator = false, decimalCount =0;
 
 setEventListeners(buttons);
 
-function setEventListeners(buttons){}
-for (let button of buttons) {
-    if (button.classList.contains('number')) {
-        button.addEventListener('click', () => {
-            numberPress(button);
-        });
-    } else if (button.classList.contains('operator')) {
-        button.addEventListener('click', () => {
-            operatorPress(button);
-        });
-    } else if (button.classList.contains('equals')) {
-        button.addEventListener('click', () => {
-            equalsPress(button);
-        });
-    } else if (button.classList.contains('clear')) {
-        button.addEventListener('click', () => {
-            clearPress(button);
-        });
-    } else {
-        button.addEventListener('click', () => {
-            powerPress(button);
-        });
+function setEventListeners(buttons) {
+    for (let button of buttons) {
+        if (button.classList.contains('number')) {
+            button.addEventListener('click', () => {
+                numberPress(button);
+            });
+        } else if (button.classList.contains('operator')) {
+            button.addEventListener('click', () => {
+                operatorPress(button);
+            });
+        } else if (button.classList.contains('equals')) {
+            button.addEventListener('click', () => {
+                equalsPress(button);
+            });
+        } else if (button.classList.contains('clear')) {
+            button.addEventListener('click', () => {
+                clearPress(button);
+            });
+        } else if (button.classList.contains('decimal')) {
+            button.addEventListener('click', () => {
+                decimalPress(button);
+            });
+        } else if (button.classList.contains('power')) {
+            button.addEventListener('click', () => {
+                powerPress(button);
+            });
+        }
     }
 }
 
@@ -57,11 +62,26 @@ function divide(num1, num2) {
 }
 
 function numberPress(button) {
+    numPressed = true;
+    canPressDecimal=true;
     setUpperDisplay(button.textContent);
 }
 
+function decimalPress(button) {
+    if (canPressDecimal && decimalCount==0) {
+        decimalCount++;
+        setUpperDisplay(button.textContent);
+        canPressDecimal = false;
+    }
+}
+
 function operatorPress(button) {
-    setUpperDisplay(button.textContent);
+    if (numPressed) {
+        setUpperDisplay(button.textContent);
+        numPressed = false;
+        canPressDecimal = false;
+        decimalCount=0;
+    }
 }
 
 function equalsPress() {
